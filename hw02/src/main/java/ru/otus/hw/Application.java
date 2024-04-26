3390dc795ff54ed5e1b31e1068de42867c701129
crazymax777m <maximus777m@mail.ru>
2024-04-26 21:36:30 +0300

    PUT Application.java

--- hw02/src/main/java/ru/otus/hw/Application.java
+++ hw02/src/main/java/ru/otus/hw/Application.java
@@ -2,16 +2,18 @@ package ru.otus.hw;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import ru.otus.hw.service.TestRunnerService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
 
