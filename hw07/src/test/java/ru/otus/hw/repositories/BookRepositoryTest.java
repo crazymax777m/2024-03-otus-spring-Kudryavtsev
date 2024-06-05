150d1588b4f74c257c674e19bdcc492f2e186c36
crazymax777m <maximus777m@mail.ru>
2024-06-05 21:50:21 +0300

    REMOVE @ExtendWith annotations

--- hw07/src/test/java/ru/otus/hw/repositories/BookRepositoryTest.java
+++ hw07/src/test/java/ru/otus/hw/repositories/BookRepositoryTest.java
@@ -1,17 +1,14 @@
 package ru.otus.hw.repositories;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class BookRepositoryTest {
 
